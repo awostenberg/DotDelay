89cbd3b3db3818cadbfd288f738f1fabc6e90995
J. Wostenberg <awosty@gmail.com>
2012-11-21 12:14:42 -0700

    Flashes can actually draw now

--- DotDelay.Lib/Flash.fs
+++ DotDelay.Lib/Flash.fs
@@ -6,10 +6,13 @@ open Microsoft.Xna.Framework.Graphics
 type Flash(game, pos : Vector2) =
   inherit GameObject(game)
   let mutable image : Texture2D = null
-  let spriteBatch = new SpriteBatch(game.GraphicsDevice)
+  let mutable spriteBatch = null
   let position = pos
+  override this.Initialize() =
+    spriteBatch <- new SpriteBatch(game.GraphicsDevice)
   override this.LoadContent() =
     image <- game.Content.Load<Texture2D>("dot")
   override this.Draw(gameTime) =
     spriteBatch.Begin()
     spriteBatch.Draw(image, position, Color.White)
+    spriteBatch.End()
--- DotDelay.Lib/MainGame.fs
+++ DotDelay.Lib/MainGame.fs
@@ -4,15 +4,29 @@ open Microsoft.Xna.Framework
 type MainGame() as this =
   inherit Game()
   let _graphics = new GraphicsDeviceManager(this)
+  let flash = new Flash(this, new Vector2(100.0f, 100.0f))
   // MUST come after the let binding, or the let binding is excecuted after the do, and the do is not executed in a fully initialized object.
   do
     this.Content.RootDirectory <- "Content"
     this.graphics.IsFullScreen <- false
+    this.setSize(new Vector2(1028.0f, 720.0f))
     this.IsMouseVisible <- true
     this.Window.Title <- "DotDelay Experiment"
+  member this.setSize(size : Vector2) =
+    this.graphics.PreferredBackBufferWidth <- (size.X |> int)
+    this.graphics.PreferredBackBufferHeight <- (size.Y |> int)
+    try this.graphics.ApplyChanges() with | :? System.NullReferenceException -> ()
   member this.graphics with get() : GraphicsDeviceManager = _graphics
-  override this.Initialize() = base.Initialize()
-  override this.Update(gameTime) = base.Update(gameTime)
+  override this.Initialize() =
+    flash.Initialize()
+    base.Initialize()
+  override this.LoadContent() =
+    flash.LoadContent()
+    base.LoadContent()
+  override this.Update(gameTime) =
+    flash.Update(gameTime)
+    base.Update(gameTime)
   override this.Draw(gameTime) =
     this.graphics.GraphicsDevice.Clear(Color.CornflowerBlue)
+    flash.Draw(gameTime)
     base.Draw(gameTime)

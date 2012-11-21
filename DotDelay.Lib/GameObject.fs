e25e94572b5971c82d66eee76c3e17a59199a8fa
J. Wostenberg <awosty@gmail.com>
2012-11-21 11:22:49 -0700

    Marked GameObject abstract and added Initialize, LoadContent, Update, and Draw

--- DotDelay.Lib/GameObject.fs
+++ DotDelay.Lib/GameObject.fs
@@ -1,4 +1,14 @@
 namespace DotDelay.Lib
 open Microsoft.Xna.Framework
 
-type GameObject(game : Game) = class end
+[<AbstractClass>]
+type GameObject(game : Game) =
+  member this.game = game
+  // Initialize other game object here -- called after LoadContent
+  abstract Initialize : unit default this.Initialize = ()
+  // Load all resources
+  abstract LoadContent : unit default this.LoadContent = ()
+  // Execute computational code
+  abstract Update : GameTime -> unit default this.Update(gameTime) = ()
+  // Draw the object's state
+  abstract Draw : GameTime -> unit default this.Draw(gameTime) = ()

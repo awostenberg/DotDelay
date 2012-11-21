658af4418088ef0eee85e272a9e25532230550f2
J. Wostenberg <awosty@gmail.com>
2012-11-21 11:42:19 -0700

    Flashes should now be able to draw

--- DotDelay.Lib/Flash.fs
+++ DotDelay.Lib/Flash.fs
@@ -1,7 +1,15 @@
 namespace DotDelay.Lib
 open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Content
+open Microsoft.Xna.Framework.Graphics
 
-type Flash(game, x : int, y : int) =
+type Flash(game, pos : Vector2) =
   inherit GameObject(game)
-  member this.X = x
-  member this.Y = y
+  let mutable image : Texture2D = null
+  let spriteBatch = new SpriteBatch(game.GraphicsDevice)
+  let position = pos
+  override this.LoadContent() =
+    image <- game.Content.Load<Texture2D>("dot")
+  override this.Draw(gameTime) =
+    spriteBatch.Begin()
+    spriteBatch.Draw(image, position, Color.White)
--- DotDelay.Lib/GameObject.fs
+++ DotDelay.Lib/GameObject.fs
@@ -5,9 +5,9 @@ open Microsoft.Xna.Framework
 type GameObject(game : Game) =
   member this.game = game
   // Initialize other game object here -- called after LoadContent
-  abstract Initialize : unit default this.Initialize = ()
+  abstract Initialize : unit -> unit default this.Initialize() = ()
   // Load all resources
-  abstract LoadContent : unit default this.LoadContent = ()
+  abstract LoadContent : unit -> unit default this.LoadContent() = ()
   // Execute computational code
   abstract Update : GameTime -> unit default this.Update(gameTime) = ()
   // Draw the object's state

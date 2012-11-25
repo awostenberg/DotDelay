b5540bacbffc800faf38985cf211c7fcb44e9b19
J. Wostenberg <awosty@gmail.com>
2012-11-24 21:08:39 -0700

    Made flashes decay and completely disappear after 25 frames

--- DotDelay.Lib/Flash.fs
+++ DotDelay.Lib/Flash.fs
@@ -7,9 +7,12 @@ open Microsoft.Xna.Framework.Graphics
 type Flash(game, pos : Vector2) =
   inherit GameObject(game)
   
+  static let flashFrames = 25
+  
   let mutable image : Texture2D = null
   let mutable spriteBatch = null
   let position = pos
+  let mutable age = 0
   
   override this.Initialize() =
     spriteBatch <- new SpriteBatch(game.GraphicsDevice)
@@ -17,7 +20,11 @@ type Flash(game, pos : Vector2) =
   override this.LoadContent() =
     image <- game.Content.Load<Texture2D>("dot")
   
+  override this.Update(gameTime) =
+    if age < flashFrames then age <- age + 1
+  
   override this.Draw(gameTime) =
     spriteBatch.Begin()
-    spriteBatch.Draw(image, new Vector2(position.X - (image.Width / 2 |> float32), position.Y - (image.Height / 2 |> float32)), Color.White)
+    let transparency = ((flashFrames |> float32) - (age |> float32)) / (flashFrames |> float32)//(flashFrames |> float32) / (age |> float32)
+    spriteBatch.Draw(image, new Vector2(position.X - (image.Width / 2 |> float32), position.Y - (image.Height / 2 |> float32)), Color.White * transparency)
     spriteBatch.End()

0ec049aff1deadedcb63df325c3ea47da8e4a9c2
J. Wostenberg <awosty@gmail.com>
2012-12-11 20:29:03 -0700

    Added a class (FlashTracker) to delegate keeping track of flashes and the delays before them

--- DotDelay.Lib/FlashTracker.fs
+++ DotDelay.Lib/FlashTracker.fs
@@ -1,1 +1,19 @@
+namespace DotDelay.Lib
+open Microsoft.Xna.Framework
 
+[<AllowNullLiteral>]
+type FlashTracker(game, pos : Vector2) =
+  inherit GameObject(game)
+  
+  let mutable flash : Flash = null
+  
+  member this.StartFlash() =
+    flash <- new Flash(game, pos)
+    flash.Initialize()
+    flash.LoadContent()
+  
+  override this.Update(gameTime) =
+    if not (flash = null) then flash.Update(gameTime)
+  
+  override this.Draw(gameTime) =
+    if not (flash = null) then flash.Draw(gameTime)
